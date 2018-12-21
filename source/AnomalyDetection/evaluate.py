1fcbb09693dea8b25a72d48c534cd4b22c1f59dd
Yury Kasimov <yury.kasimov@dhcp-83-211.felk.cvut.cz>
2018-12-21 13:23:10 +0100

    add reporting to the evaluation

--- source/AnomalyDetection/evaluate.py
+++ source/AnomalyDetection/evaluate.py
@@ -9,14 +9,17 @@ import json
 
 def evaluate(models, scalers, features, verbose=False):
     predictions = []
+    scores = []
     for feature_name in global_features:
         X = scalers[feature_name].transform(features[feature_name])
-        prediction = models[feature_name]._predict(X)
+        prediction = models[feature_name].predict(X)
+        score = models[feature_name].score_samples(X)
         if verbose:
             print(f'Feature prediction: {feature_name}')
             print(prediction)
         predictions.append(prediction)
-    return predictions
+        scores.append(score)
+    return predictions, scores
 
 
 def _load_models(folder):
@@ -24,7 +27,8 @@ def _load_models(folder):
     for feature_name in global_features:
         path = f'{folder}/{feature_name}_model.pkl'
         with open(path, 'rb') as f:
-            models[feature_name] = pickle.load(f)
+            models[feature_name] = pickle.load(f).set_params(novelty=True)
+
     return models
 
 
@@ -38,14 +42,15 @@ def _load_scalers(base_folder):
     return scalers
 
 
-def _report(predictions, dates):
+def _report(predictions, scores, dates):
     output_dict = {}
-    for pred, feat in zip(predictions, global_features):
+    for pred, score, feat in zip(predictions, scores, global_features):
         idx = np.where(pred == -1)[0]
         anomalous_windows = [dates[i] for i in idx]
-        for w in anomalous_windows:
+        scores_windows = [score[i] for i in idx]
+        for w, s in zip(anomalous_windows, scores_windows):
             tmp_list = output_dict.get(w, [])
-            tmp_list.append(f'{feat} : -1')
+            tmp_list.append(f'{feat} : {s}')
             output_dict[w] = tmp_list
     return output_dict
 
@@ -54,11 +59,10 @@ if __name__ == '__main__':
     # global_features.remove('clientDestinationPortTotalBytesUDPEstablished')
     parameters = parser_inference.parse_args(sys.argv[1:])
     features, dates = get_data(parameters.profiles, feature_names=global_features)
-    print(dates)
     models = _load_models(parameters.models)
     scalers = _load_scalers(parameters.models)
-    predictions = evaluate(models, scalers, features, parameters.verbose)
-    report = _report(predictions, dates)
+    predictions, scores = evaluate(models, scalers, features, parameters.verbose)
+    report = _report(predictions, scores, dates)
     with open('report.json', 'w') as fp:
         json.dump(report, fp)
     final_prediction = majority_voting(predictions)
